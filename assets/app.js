e0b498f5c35fc19dd17f6b625674eaee47bb13ae
QuentinBastos <quentin.bastos@etu.univ-lyon1.fr>
2025-01-26 16:57:38 +0100

    feat(symfony): project html to symfony

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+import './bootstrap.js';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+import './css/style.css';
+import './css/background.css';
+import './css/basic-rule.css';
+import './css/card.css';
+import './css/particles.css';
+import './css/projects.css';
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
